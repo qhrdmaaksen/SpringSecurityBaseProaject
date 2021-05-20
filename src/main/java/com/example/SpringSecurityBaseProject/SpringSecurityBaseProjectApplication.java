8041998a61a710c1063a326492d09506946638b1
vitamin7777777 <rlaalsdn8@gmail.com>
2021-05-20 22:59:26 +0900

    setup project

---
+++ src/main/java/com/example/SpringSecurityBaseProject/SpringSecurityBaseProjectApplication.java
@@ -0,0 +1,11 @@
+package com.example.SpringSecurityBaseProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityBaseProjectApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityBaseProjectApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/SpringSecurityBaseProject/SpringSecurityBaseProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringSecurityBaseProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityBaseProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
